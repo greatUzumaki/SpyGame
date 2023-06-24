e9551ad336c68cce6c04493c1e35b639ab8c5f33
Alexey Pargachev <pargachev20@gmail.com>
2023-06-25 02:10:11 +0700

    added vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
 
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [preact()],
-    base: '/SpyGame/',
-  };
-
-  if (command !== 'serve') {
-    config.base = '/SpyGame/';
-  }
-
-  return config;
+export default defineConfig({
+  plugins: [preact()],
 });
