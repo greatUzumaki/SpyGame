f27eda0b6d6bf791b14c61972560b0f0608eff2d
Alexey Pargachev <pargachev20@gmail.com>
2023-06-25 15:45:10 +0700

    disable zoom in ios

--- src/utils/setRoles.ts
+++ src/utils/setRoles.ts
@@ -1,11 +1,9 @@
 export function setRoles(playersLen: number, roles: string[]) {
-  const randomRoles = [...roles]; // Копирование массива ролей
+  const randomRoles = [...roles];
 
-  // Добавление случайной роли "Шпион" в массив ролей
   const spyIndex = Math.floor(Math.random() * playersLen);
   randomRoles[spyIndex] = 'Шпион';
 
-  // Заполнение оставшихся ролей случайными значениями
   for (let i = roles.length; i < playersLen; i++) {
     const randomIndex = Math.floor(Math.random() * roles.length);
     randomRoles.push(roles[randomIndex]);
